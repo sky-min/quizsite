bc4439f5e1a4c076bb62fd852c6e9b6d7208fd1e
skymin <ppt00003333@gmail.com>
2021-12-19 23:55:22 +0900

    주석

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.prototype.$Q = ['타이타닉의 구명 보트에는 몇 명이 탈수 있을까?'
+Vue.prototype.$Q = [ //문제
+	'타이타닉의 구명 보트에는 몇 명이 탈수 있을까?'
 ];
-Vue.prototype.$A = [
+Vue.prototype.$A = [ //정답이라 인정할 값
 	['9명', '9']
 ];
-Vue.prototype.$H = [
+Vue.prototype.$H = [ //틀렸을때 출력할 답
 	'9명(구명 보트)'
 ];
 
